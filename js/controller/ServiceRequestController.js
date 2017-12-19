c52dd09fb2449200c1e30cb602df576baca99f1d
Prashant Iyengar <prashantiyenger@gmail.com>
2017-12-19 20:59:46 +0530

    Calling rest api to send emails

--- js/controller/ServiceRequestController.js
+++ js/controller/ServiceRequestController.js
@@ -11,9 +11,14 @@ angular.module("LifeCareApp").controller("ContactController", function ($scope, 
         $scope.request.requestId = generateRequestId();
         //$scope.request.date = new Date();
         console.log($scope.request);
-        ServiceRequestFactory.createServiceRequest($scope.request).then(function(){
+        ServiceRequestFactory.createServiceRequest($scope.request).then(function(response){
             console.log("Service request created successfully. ");
             $scope.succesMessage = "Congratulations, Service request created successfully.";
+            ServiceRequestFactory.sendEmail(response.data).then(function () {
+                console.log("Emails sent successfully. ");
+            }, function () {
+                console.log("Error occured while sending emails");
+            });
         }, function () {
             console.log("Error occured while creating service request ");
             $scope.errorMessage = "Something went wrong!!! Please try again.";
